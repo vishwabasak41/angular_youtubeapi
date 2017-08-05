93353e1fa4bc2537aafad7687b42bc3834ca9c94
vishwabasak41 <vishwabasak95@gmail.com>
2017-08-05 18:50:55 +0530

    recent changes on 5th august

--- main.js
+++ main.js
@@ -20,97 +20,37 @@ $(function(){
 
 
 		});
+	
 
 		request.execute(function(response){
 			console.log(response)
-			var result=response.result;
-			$each(result.items,function(index,item){
-				$.get("tpl/lor.html",function(data){
-					$('#result').append(tplawesome(data, [{"title":item.snippet.thumbnails.title,"videoid":item.Object[0].id.videoId}]));
+			var result=response.items;
+			console.log("result is"+response.result)
+			$.each(response.items,function(index,item){
+				$.get("lor.html",function(data){
+
+					for(i=0;i<=5;i++){
+					$('#result').append(tplawesome(data, [{"title":response.items["1"].snippet.title,"videoid":response.items["1"].id.videoId}]));
+				}
+
 				})
 			})
 		})
-	})
 
 
 
 
-
+});
 
 })
-
-
-
 
 
 function init(){
 	gapi.client.setApiKey("AIzaSyDDoQP_kbK1xeEnteCOX0C7OFKCF19OdOk");
 	gapi.client.load("youtube","v3",function(){
 
-
-//lines
-
-
-
-
 	})
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function start() {
-//   // 2. Initialize the JavaScript client library.
-//   gapi.client.init({
-//     'apiKey': 'YOUR_API_KEY',
-//     // clientId and scope are optional if auth is not required.
-//     'clientId': 'YOUR_WEB_CLIENT_ID.apps.googleusercontent.com',
-//     'scope': 'profile',
-//   }).then(function() {
-//     // 3. Initialize and make the API request.
-//     return gapi.client.request({
-//       'path': 'https://people.googleapis.com/v1/people/me?requestMask.includeField=person.names',
-//     })
-//   }).then(function(response) {
-//     console.log(response.result);
-//   }, function(reason) {
-//     console.log('Error: ' + reason.result.error.message);
-//   });
-// };
-// // 1. Load the JavaScript client library.
-// gapi.load('client', start);
-
-
-// // var obj=buildApiRequest('GET',
-// //                 '/youtube/v3/search',
-// //                 {'maxResults': '25',
-// //                  'part': 'snippet',
-// //                  'q': 'sonu nigam',
-// //                  'type': ''});
-
-
 
 
 
